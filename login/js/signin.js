81cbdd0731ee6447d144660ab46a8af7848c9a35
takahara <takaharasuzuka@gmail.com>
2024-02-06 10:18:46 +0900

    住所バリデーション修正2

--- login/js/signin.js
+++ login/js/signin.js
@@ -73,7 +73,9 @@ function signUp() {
   }
 
   //住所バリデーションチェック
-  if (!address[0].value.match(/^[!"#$%&'()\*\+\-\.,\/:;<=>?＠\[\\\]^_`{|}~]*$/i)) {
+  if (
+    !address[0].value.match(/^[!"#$%&'()\*\+\-\.,\/:;<=>?＠\[\\\]^_`{|}~]*$/i)
+  ) {
     address[0].style.backgroundColor = "#9cb6e6";
     errText[4].textContent = "";
   } else {
