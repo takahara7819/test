ec154a183adca948352b162994f456a9607d49a1
takahara <takaharasuzuka@gmail.com>
2024-03-08 15:54:42 +0900

    ログイン画面修正

--- login/js/main.js
+++ login/js/main.js
@@ -3,6 +3,7 @@ let userPass = document.getElementById("pass");
 let msgId = document.getElementById("msgId");
 let msgPs = document.getElementById("msgPs");
 
+//ログイン画面 バリデーションチェック
 function btn() {
   let re = new RegExp(/^[ -~]*$/gi);
   let searchId = userId.value;
@@ -10,7 +11,7 @@ function btn() {
 
   if (!searchId.match(/^[ -~]*$/)) {
     userId.style.backgroundColor = "red";
-    msgId.textContent = "IDへは半角英数字記号のみで記入して下さい";
+    msgId.textContent = "IDは半角英数字記号のみで記入して下さい";
   } else {
     userId.style.backgroundColor = "#9cb6e6";
     msgId.textContent = "";
@@ -18,7 +19,7 @@ function btn() {
 
   if (!searchPs.match(/^[ -~]*$/)) {
     userPass.style.backgroundColor = "red";
-    msgPs.textContent = "IDへは半角英数字記号のみで記入して下さい";
+    msgPs.textContent = "パスワードは半角英数字記号のみで記入して下さい";
   } else {
     userPass.style.backgroundColor = "#9cb6e6";
     msgPs.textContent = "";
@@ -26,8 +27,5 @@ function btn() {
 
   if (searchId.match(/^[ -~]*$/) && searchPs.match(/^[ -~]*$/)) {
     window.location.href = "/login/signin.html";
-  } else {
-    userId.style.backgroundColor = "red";
-    userPass.style.backgroundColor = "red";
   }
 }
